18336c188ca4ef4c407270100a246f1864c371a8
Bjørn Johansen <bjorn@gandalf.no>
2012-08-24 10:35:53 +0200

    Fixed correct enqueueing of JS (oops).

--- samwise-emaillink.php
+++ samwise-emaillink.php
@@ -1,30 +1,37 @@
 <?php
 
-/*
-This is for separate inclustion of the jquery plugin and the init script that acually use the plugin
+class Samwise_Emaillink {
 
-wp_enqueue_script(
-	'jquery.emaillink',
-	plugins_url( 'samwise-emaillink/jquery.emaillink.js',  __FILE__ ),
-	array('jquery'),
-	'1.4',
-	true
-);
-wp_enqueue_script(
-	'jquery.emaillink-init',
-	plugins_url( 'samwise-emaillink/jquery.emaillink-init.js',  __FILE__ ),
-	array('jquery', 'jquery.emaillink'),
-	'1.4',
-	true
-);
+	function enqueue_scripts () {
+		/*
+		This is for separate inclustion of the jquery plugin and the init script that acually use the plugin
 
-*/
+		wp_enqueue_script(
+			'jquery.emaillink',
+			plugins_url( 'samwise-emaillink/jquery.emaillink.js',  __FILE__ ),
+			array('jquery'),
+			'1.4',
+			true
+		);
+		wp_enqueue_script(
+			'jquery.emaillink-init',
+			plugins_url( 'samwise-emaillink/jquery.emaillink-init.js',  __FILE__ ),
+			array('jquery', 'jquery.emaillink'),
+			'1.4',
+			true
+		);
 
-/* Both of the above, combined to one request */
-wp_enqueue_script(
-	'jquery.emaillink',
-	plugins_url( 'samwise-emaillink/jquery.emaillink-combined.min.js',  __FILE__ ),
-	array('jquery'),
-	'1.4',
-	true
-);
+		*/
+
+		/* Both of the above, combined to one request */
+		wp_enqueue_script(
+			'jquery.emaillink',
+			plugins_url( 'samwise-emaillink/jquery.emaillink-combined.min.js',  __FILE__ ),
+			array('jquery'),
+			'1.4',
+			true
+		);
+	}
+}
+
+add_action( 'wp_enqueue_scripts', array( 'Samwise_Emaillink', 'enqueue_scripts' ) );
