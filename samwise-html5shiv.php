ce740d804211365bb25917905d931b5797aff67d
Bjørn Johansen <bjorn@gandalf.no>
2012-08-27 15:50:34 +0200

    Make sure the shiv isn't added twice

--- samwise-html5shiv.php
+++ samwise-html5shiv.php
@@ -6,6 +6,9 @@ class Samwise_Html5shiv {
 
 		echo sprintf( '<!--[if lt IE 9]><script src="%s" type="text/javascript"></script><![endif]-->', plugins_url( 'samwise-html5shiv/html5shiv.js',  __FILE__ ) );
 	
+		// wp_print_scripts seems to run in the footer even when ran in the head (tested WP 3.4.1). So remove this action when run as we really don't need it twice.
+		remove_action( 'wp_print_scripts', array( __CLASS__, 'print_script_include' ) );
+		
 	}
 }
 
